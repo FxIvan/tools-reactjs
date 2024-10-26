b6ed713150998114dd515fd4fcf2ae334bee3ecc
fxivaninfobae <ialmendra@infobae.com>
2024-10-26 19:42:30 -0300

    first commit

---
+++ app/calendario/page.jsx
@@ -0,0 +1,9 @@
+export default function Calendario() {
+
+    return(
+        <div className="container">
+            <h3>Calendario</h3>
+        </div>
+    )
+
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    sassOptions: {
+        implementation: 'sass-embedded',
+        sassOptions: {
+            includePaths: ['./styles'],
+        },
+    },
+};
 
 export default nextConfig;
